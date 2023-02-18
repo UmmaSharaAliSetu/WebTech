79e17da063b4b81ac98e110c50f2cbcc178d2eda
Umma Shara Ali Setu <102200814+UmmaSharaAliSetu@users.noreply.github.com>
2023-02-18 21:24:05 +0600

    Update setcookie.php

--- LAB4/setcookie.php
+++ LAB4/setcookie.php
@@ -1,1 +1,13 @@
-
+<?php
+if ($_SERVER['REQUEST_METHOD'] === "POST") {
+	$color = $_POST['color'];
+	$expire_time = strtotime($_POST['expire_time']);
+	setcookie("color", $color, $expire_time);
+	if (isset($_GET['page'])) {
+		header("Location: " . $_GET['page']);
+	}
+	else {
+		header("Location: cookie.php");
+	}
+}
+?>
